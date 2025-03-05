c5d2ea9eecb2052a17b5df4a6ad570ec5a04f3f1
Taoismimmortal <2696492904@qq.com>
2025-03-05 11:42:11 +0800

    feat:添加订单和商品服务

---
+++ doubao-order/src/main/java/com/doubao/order/OrderItem.java
@@ -0,0 +1,38 @@
+package com.doubao.order;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "sys_order_item")
+@Data
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long item_id;
+
+    @Column(nullable = false)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double totalAmount;
+
+    @Column(updatable = false)
+    private Date createTime;
+
+    private Date updateTime;
+}
