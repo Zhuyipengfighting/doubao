4d7528f2a1d7070cae07a9c16a7d75cb052a255e
gdsbapple <13790793568@stu.gdou.edu.cn>
2025-03-05 14:43:10 +0800

    feat: 服务注册与发现

--- doubao-admin/src/main/java/com/doubao/RuoYiApplication.java
+++ doubao-admin/src/main/java/com/doubao/RuoYiApplication.java
@@ -3,6 +3,7 @@ package com.doubao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 启动程序
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author doubao
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
 public class RuoYiApplication
 {
     public static void main(String[] args)
